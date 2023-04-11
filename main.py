c791433429a423d47cd23f782094e62fb3d5dc19
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-10 23:35:45 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#O app utilizou os seguintes modulos flask, flask-login, flask-sqlalchemy
