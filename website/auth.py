c02a91e042aba468d95274c7532e81788f6cc6ac
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-13 19:45:37 -0300

    Criação do app de anotações Utilizando o post para enviar o formulário (começo)

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", bolean=False)
 
 @auth.route('/logout')
 def logout():
     return "<p>Deslogar </p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
         return render_template("sign-up.html")
