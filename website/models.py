8531713327b44b3c8d37a26f92a93cd1c9aa2bc5
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-18 21:51:53 -0300

    Criação do app de anotações Relacionamentos de Chave Estrangeira

--- website/models.py
+++ website/models.py
@@ -6,9 +6,11 @@ class note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     data = db.column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.string(150), unique = True)
     password = db.Column(db.string(150))
     Primeiro_nome = db.Column(db.string(150))
+    notes = db.relationship('Note')
