5940ad1b16276df2465646369b070a424b6c3152
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-20 18:34:03 -0300

    Correção de bugs e login/cadastro funcionais

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 #O app utilizou os seguintes modulos flask, flask-login, flask-sqlalchemy
--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,38 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html", bolean=False)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logado com sucesso!!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Senha incorreta, tente novamente.', category='error')
+        else:
+            flash('Email não existe.', category='error')
+
+    return render_template("login.html", user=current_user)
+
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Deslogar </p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -22,23 +41,24 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+       
+        if user:
+            flash('Email já existe', category='error')
+        elif len(email) < 4:
             flash('O email tem que ser maior que 3 cracteres', category='error')
         elif len(first_name) < 2:
-            flash('O nome tem que ser maior que 1 cracteres', category='error')
+            flash('O nome tem que ser maior que 1 caracteres', category='error')
         elif password1 != password2:
             flash('Senhas diferentes', category='error')
         elif len(password1) < 7:
-            flash('A senha tem que ser maior que 7 cracteres', category='error')
+            flash('A senha tem que ser maior que 7 caracteres', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
-            flash('Conta criada!!!', category='success')
+            db.session.commit()  
+            flash('Account created!', category='success')
             return redirect(url_for('views.home'))
-           #adiciona o usuário no banco de dados
-           
-    
-    return render_template("sign-up.html")
 
+    return render_template("sign-up.html")
