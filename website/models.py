9908d6fa5839f03a1f15a2a0f6fa3959daed39a3
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-19 22:34:43 -0300

    Criação do app de anotações Criação do banco de dados

--- website/models.py
+++ website/models.py
@@ -2,15 +2,15 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class note(db.Model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    data = db.column(db.DateTime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.string(150), unique = True)
-    password = db.Column(db.string(150))
-    Primeiro_nome = db.Column(db.string(150))
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    Primeiro_nome = db.Column(db.String(150))
     notes = db.relationship('Note')
