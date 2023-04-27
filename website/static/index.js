2f5fe4ab435ec851a0aaaa690fcdf6251cb89f78
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-04-26 23:18:51 -0300

    Deletar anotações funcional E correção de bugs

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,12 @@
-function deleteNote(noteId) {
+/* function deleteNote(noteId) {
     fexth('/delete-note', {
         method: "POST",
-        body: JSON.stringify({ noteId: noteId}),
+        body: JSON.stringify({ noteId: noteId }),
     }).then((_res) => {
         window.location.href ="/"
     })
-}
+} 
+
+Não será utilizado o js que está aqui, 
+será utilizado o que está no arquivo base.html
+*/
